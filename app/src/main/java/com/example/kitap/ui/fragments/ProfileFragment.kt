54d18a2cd1085fd24afe7842523768c8f6207830
nuka000 <nurkanat.03wf@mail.ru>
2024-04-03 17:23:19 +0600

    added ui settings

--- app/src/main/java/com/example/kitap/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/kitap/ui/fragments/ProfileFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.kitap.R
 import com.example.kitap.databinding.FragmentLoginBinding
 import com.example.kitap.databinding.FragmentProfileBinding
 
@@ -16,5 +18,15 @@ class ProfileFragment: Fragment(){
     ): View? {
         binding = FragmentProfileBinding.inflate(layoutInflater,container,false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.backButton.setOnClickListener {
+            findNavController().navigateUp()
+        }
+        binding.settingsButton.setOnClickListener {
+            findNavController().navigate(R.id.action_profileFragment_to_settingsFragment)
+        }
     }
 }
